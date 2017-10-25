8f3e06e66c352f4f7381b8c34fff76361236854a
zachion <zioannou77@gmail.com>
2017-10-25 17:13:32 +0300

    edit individual items

--- SolutionName/SolutionName.Web/Scripts/salesorderviewmodel.js
+++ SolutionName/SolutionName.Web/Scripts/salesorderviewmodel.js
@@ -5,13 +5,14 @@
     Deleted: 3
 };
 
+
 var salesOrderItemMapping = {
     'SalesOrderItems': {
         key: function (salesOrderItem) {
             return ko.utils.unwrapObservable(salesOrderItem.SalesOrderItemId);
         },
         create: function (options) {
-            return ko.utils.unwrapObservable(options.data);
+            return new SalesOrderItemViewModel(options.data);
         }
     }
 };
@@ -20,6 +21,18 @@ var salesOrderItemMapping = {
 SalesOrderItemViewModel = function (data) {
     var self = this;
     ko.mapping.fromJS(data, salesOrderItemMapping, self);
+
+    self.flagSalesOrderItemAsEdited = function () {
+        if (self.ObjectState() != ObjectState.Added) {
+            self.ObjectState(ObjectState.Modified);
+        }
+
+        return true;
+    },
+
+        self.ExtendedPrice = ko.computed(function () {
+            return (self.Quantity() * self.UnitPrice()).toFixed(2);
+        });
 };
 
 
@@ -54,5 +67,13 @@ SalesOrderViewModel = function (data) {
         self.addSalesOrderItem = function () {
             var salesOrderItem = new SalesOrderItemViewModel({ SalesOrderItemId: 0, ProductCode: "", Quantity: 1, UnitPrice: 0, ObjectState: ObjectState.Added });
             self.SalesOrderItems.push(salesOrderItem);
-        };
+        },
+
+        self.Total = ko.computed(function () {
+            var total = 0;
+            ko.utils.arrayForEach(self.SalesOrderItems(), function (salesOrderItem) {
+                total += parseFloat(salesOrderItem.ExtendedPrice());
+            });
+            return total.toFixed(2);
+        });
 };
